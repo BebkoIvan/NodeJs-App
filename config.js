275fdb3aa1ba17f64fb32b27301b6e64ff002c0b
BebkoIvan <bebkovan@gmail.com>
2020-02-01 22:40:35 +0200

    FaceBook auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1112186402447246',
+        clientSecret: '6a18cda8d317ef655b67828d8427115b'
+    }
 }
